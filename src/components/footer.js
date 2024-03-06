998f9133c24bf66363900a58ccfc4e3911cf8fde
maitra-prashant <code.prashant001@gmail.com>
2024-03-07 01:32:40 +0530

    body added

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function footer() {
     return (
         <div>
-            {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
             omnis nihil, illum repudiandae recusandae eos atque eum asperiores
             obcaecati perspiciatis veniam aliquam, ipsa eaque, dolor maiores
             amet accusamus vero? Ab, totam. Quidem corporis at odit itaque
@@ -14,7 +14,7 @@ export default function footer() {
             consequatur eaque pariatur eius neque consectetur deleniti,
             molestiae alias excepturi officiis doloremque dolorem, placeat at
             sunt iusto amet aut exercitationem atque tempora dolorum? Quo magnam
-            deleniti quis deserunt sunt atque at recusandae? */}
+            deleniti quis deserunt sunt atque at recusandae?
         </div>
     );
 }
